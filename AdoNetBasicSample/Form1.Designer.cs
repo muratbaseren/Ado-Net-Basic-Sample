bf51d325a6f048dc2736b503b0f85580754b4021
muratbaseren <muratbaseren@gmail.com>
2016-07-26 13:07:07 +0300

    Add project files.

---
+++ AdoNetBasicSample/Form1.Designer.cs
@@ -0,0 +1,278 @@
+﻿namespace AdoNetBasicSample
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lstKisiler = new System.Windows.Forms.ListBox();
+            this.btnVerileriGetir = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtAd = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtSoyad = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtTelefon = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtEposta = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtTcNo = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtAdres = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.dtpDogumTarihi = new System.Windows.Forms.DateTimePicker();
+            this.btnEkle = new System.Windows.Forms.Button();
+            this.btnGuncelle = new System.Windows.Forms.Button();
+            this.btnSil = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lstKisiler
+            // 
+            this.lstKisiler.FormattingEnabled = true;
+            this.lstKisiler.ItemHeight = 18;
+            this.lstKisiler.Location = new System.Drawing.Point(13, 13);
+            this.lstKisiler.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.lstKisiler.Name = "lstKisiler";
+            this.lstKisiler.Size = new System.Drawing.Size(195, 328);
+            this.lstKisiler.TabIndex = 0;
+            // 
+            // btnVerileriGetir
+            // 
+            this.btnVerileriGetir.Location = new System.Drawing.Point(215, 12);
+            this.btnVerileriGetir.Name = "btnVerileriGetir";
+            this.btnVerileriGetir.Size = new System.Drawing.Size(75, 55);
+            this.btnVerileriGetir.TabIndex = 1;
+            this.btnVerileriGetir.Text = "Verileri Getir";
+            this.btnVerileriGetir.UseVisualStyleBackColor = true;
+            this.btnVerileriGetir.Click += new System.EventHandler(this.btnVerileriGetir_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(296, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(25, 18);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Ad";
+            // 
+            // txtAd
+            // 
+            this.txtAd.Location = new System.Drawing.Point(379, 10);
+            this.txtAd.MaxLength = 20;
+            this.txtAd.Name = "txtAd";
+            this.txtAd.Size = new System.Drawing.Size(158, 25);
+            this.txtAd.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(296, 45);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(48, 18);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Soyad";
+            // 
+            // txtSoyad
+            // 
+            this.txtSoyad.Location = new System.Drawing.Point(379, 42);
+            this.txtSoyad.MaxLength = 20;
+            this.txtSoyad.Name = "txtSoyad";
+            this.txtSoyad.Size = new System.Drawing.Size(158, 25);
+            this.txtSoyad.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(291, 76);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(57, 18);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Telefon";
+            // 
+            // txtTelefon
+            // 
+            this.txtTelefon.Location = new System.Drawing.Point(379, 73);
+            this.txtTelefon.MaxLength = 15;
+            this.txtTelefon.Name = "txtTelefon";
+            this.txtTelefon.Size = new System.Drawing.Size(158, 25);
+            this.txtTelefon.TabIndex = 3;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(296, 107);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(52, 18);
+            this.label4.TabIndex = 2;
+            this.label4.Text = "EPosta";
+            // 
+            // txtEposta
+            // 
+            this.txtEposta.Location = new System.Drawing.Point(379, 104);
+            this.txtEposta.MaxLength = 60;
+            this.txtEposta.Name = "txtEposta";
+            this.txtEposta.Size = new System.Drawing.Size(158, 25);
+            this.txtEposta.TabIndex = 3;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(296, 138);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(43, 18);
+            this.label5.TabIndex = 2;
+            this.label5.Text = "TcNo";
+            // 
+            // txtTcNo
+            // 
+            this.txtTcNo.Location = new System.Drawing.Point(379, 135);
+            this.txtTcNo.MaxLength = 11;
+            this.txtTcNo.Name = "txtTcNo";
+            this.txtTcNo.Size = new System.Drawing.Size(158, 25);
+            this.txtTcNo.TabIndex = 3;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(299, 169);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(45, 18);
+            this.label6.TabIndex = 2;
+            this.label6.Text = "Adres";
+            // 
+            // txtAdres
+            // 
+            this.txtAdres.Location = new System.Drawing.Point(379, 166);
+            this.txtAdres.MaxLength = 250;
+            this.txtAdres.Name = "txtAdres";
+            this.txtAdres.Size = new System.Drawing.Size(158, 25);
+            this.txtAdres.TabIndex = 3;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(290, 205);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(83, 18);
+            this.label7.TabIndex = 2;
+            this.label7.Text = "Doğum Tar";
+            // 
+            // dtpDogumTarihi
+            // 
+            this.dtpDogumTarihi.CustomFormat = "dd/MM/yyyy";
+            this.dtpDogumTarihi.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
+            this.dtpDogumTarihi.Location = new System.Drawing.Point(379, 200);
+            this.dtpDogumTarihi.Name = "dtpDogumTarihi";
+            this.dtpDogumTarihi.Size = new System.Drawing.Size(158, 25);
+            this.dtpDogumTarihi.TabIndex = 4;
+            // 
+            // btnEkle
+            // 
+            this.btnEkle.Location = new System.Drawing.Point(462, 231);
+            this.btnEkle.Name = "btnEkle";
+            this.btnEkle.Size = new System.Drawing.Size(75, 52);
+            this.btnEkle.TabIndex = 5;
+            this.btnEkle.Text = "Ekle";
+            this.btnEkle.UseVisualStyleBackColor = true;
+            this.btnEkle.Click += new System.EventHandler(this.btnEkle_Click);
+            // 
+            // btnGuncelle
+            // 
+            this.btnGuncelle.Location = new System.Drawing.Point(381, 231);
+            this.btnGuncelle.Name = "btnGuncelle";
+            this.btnGuncelle.Size = new System.Drawing.Size(75, 52);
+            this.btnGuncelle.TabIndex = 5;
+            this.btnGuncelle.Text = "Güncelle";
+            this.btnGuncelle.UseVisualStyleBackColor = true;
+            this.btnGuncelle.Click += new System.EventHandler(this.btnGuncelle_Click);
+            // 
+            // btnSil
+            // 
+            this.btnSil.Location = new System.Drawing.Point(300, 231);
+            this.btnSil.Name = "btnSil";
+            this.btnSil.Size = new System.Drawing.Size(75, 52);
+            this.btnSil.TabIndex = 5;
+            this.btnSil.Text = "Sil";
+            this.btnSil.UseVisualStyleBackColor = true;
+            this.btnSil.Click += new System.EventHandler(this.btnSil_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(549, 361);
+            this.Controls.Add(this.btnSil);
+            this.Controls.Add(this.btnGuncelle);
+            this.Controls.Add(this.btnEkle);
+            this.Controls.Add(this.dtpDogumTarihi);
+            this.Controls.Add(this.txtAdres);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.txtTcNo);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.txtEposta);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtTelefon);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtSoyad);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtAd);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnVerileriGetir);
+            this.Controls.Add(this.lstKisiler);
+            this.Font = new System.Drawing.Font("Tahoma", 11F);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "Form1";
+            this.Text = "ADO.NET Basic Sample";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox lstKisiler;
+        private System.Windows.Forms.Button btnVerileriGetir;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtAd;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtSoyad;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtTelefon;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtEposta;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtTcNo;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txtAdres;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.DateTimePicker dtpDogumTarihi;
+        private System.Windows.Forms.Button btnEkle;
+        private System.Windows.Forms.Button btnGuncelle;
+        private System.Windows.Forms.Button btnSil;
+    }
+}
+
---
+++ AdoNetBasicSample/Kisi.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdoNetBasicSample
+{
+    // Test 1
+    // Veritabanındaki kişiler tablosundaki satırları temsil eden nesne
+    public class Kisi
+    {
+        public int Id { get; set; }
+        public string Ad { get; set; }
+        public string Soyad { get; set; }
+        public string TcNo { get; set; }
+        public string EPosta { get; set; }
+        public string Adres { get; set; }
+        public string Telefon { get; set; }
+        public DateTime DogumTarihi { get; set; }
+
+        public override string ToString()
+        {
+            return Ad + " " + Soyad;
+        }
+    }
+}
